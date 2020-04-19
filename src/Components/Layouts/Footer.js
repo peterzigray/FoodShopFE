9de359cdeb8b7717a07c23f462f456d187868e22
splitSlice <zigray.peter@gmail.com>
2020-04-19 17:24:27 +0200

    footer completed

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import logo from '../../assets/logo.svg';
 import Grid from '@material-ui/core/Grid';
+// icons
+import Hidden from '@material-ui/core/Hidden';
+import facebook from '../../assets/facebook.svg';
+import instagram from '../../assets/instagram.svg';
+import twitter from '../../assets/twitter.svg';
 
 const useStyles = makeStyles((theme) => ({
   footer: {
@@ -12,13 +17,13 @@ const useStyles = makeStyles((theme) => ({
     position: 'relative',
   },
   adorement: {
-    width: '25em',
+    width: '15em',
     verticalAlign: 'bottom',
     [theme.breakpoints.down('md')]: {
-      width: '21em',
+      width: '10em',
     },
     [theme.breakpoints.down('xs')]: {
-      width: '15em',
+      width: '9em',
     },
   },
   mainContainer: {
@@ -34,82 +39,139 @@ const useStyles = makeStyles((theme) => ({
   gridItem: {
     margin: '3em',
   },
+  icon: {
+    height: '4em',
+    width: '4em',
+    [theme.breakpoints.down('xs')]: {
+      height: '2.5em',
+      width: '2.5em',
+    },
+  },
+  socialContainer: {
+    position: 'absolute',
+    marginTop: '-6em',
+    right: '1.5em',
+    [theme.breakpoints.down('xs')]: {
+      right: '0.6em',
+    },
+  },
 }));
 export default function Footer() {
   const classes = useStyles();
   return (
     <footer className={classes.footer}>
-      <Grid container justify="center" className={classes.mainContainer}>
-        <Grid item className={classes.gridItem}>
-          <Grid container direction="column" spacing={2}>
-            <Grid item component={Link} to="/" className={classes.link}>
-              {' '}
-              Home
+      <Hidden mdDown>
+        <Grid container justify="center" className={classes.mainContainer}>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item component={Link} to="/" className={classes.link}>
+                {' '}
+                Home
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                component={Link}
+                to="/services"
+                className={classes.link}
+              >
+                {' '}
+                Services
+              </Grid>
+              <Grid item component={Link} to="/" className={classes.link}>
+                {' '}
+                Custom SW development
+              </Grid>
+              <Grid item component={Link} to="/" className={classes.link}>
+                {' '}
+                Mobile App Development
+              </Grid>
+              <Grid item component={Link} to="/" className={classes.link}>
+                {' '}
+                Webside Development
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item component={Link} to="/" className={classes.link}>
+                {' '}
+                The revolution
+              </Grid>
+              <Grid item component={Link} to="/" className={classes.link}>
+                {' '}
+                Vision
+              </Grid>
+              <Grid item component={Link} to="/" className={classes.link}>
+                {' '}
+                Technology
+              </Grid>
+              <Grid item component={Link} to="/" className={classes.link}>
+                {' '}
+                Process
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item component={Link} to="/" className={classes.link}>
+                About Us
+              </Grid>
+              <Grid item component={Link} to="/" className={classes.link}>
+                History
+              </Grid>
+              <Grid item component={Link} to="/" className={classes.link}>
+                Team
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item className={classes.link}>
+                Contact Us
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item className={classes.gridItem}>
-          <Grid container direction="column" spacing={2}>
-            <Grid item component={Link} to="/services" className={classes.link}>
-              {' '}
-              Services
-            </Grid>
-            <Grid item component={Link} to="/" className={classes.link}>
-              {' '}
-              Custom SW development
-            </Grid>
-            <Grid item component={Link} to="/" className={classes.link}>
-              {' '}
-              Mobile App Development
-            </Grid>
-            <Grid item component={Link} to="/" className={classes.link}>
-              {' '}
-              Webside Development
-            </Grid>
-          </Grid>
+      </Hidden>
+      <img alt="icecone" className={classes.adorement} src={logo}></img>
+      <Grid
+        container
+        className={classes.socialContainer}
+        spacing={2}
+        justify="flex-end"
+      >
+        <Grid
+          item
+          component={'a'}
+          href="https://www.facebook.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img alt="facebook logo" src={facebook} className={classes.icon} />
         </Grid>
-        <Grid item className={classes.gridItem}>
-          <Grid container direction="column" spacing={2}>
-            <Grid item component={Link} to="/" className={classes.link}>
-              {' '}
-              The revolution
-            </Grid>
-            <Grid item component={Link} to="/" className={classes.link}>
-              {' '}
-              Vision
-            </Grid>
-            <Grid item component={Link} to="/" className={classes.link}>
-              {' '}
-              Technology
-            </Grid>
-            <Grid item component={Link} to="/" className={classes.link}>
-              {' '}
-              Process
-            </Grid>
-          </Grid>
+        <Grid
+          item
+          component={'a'}
+          href="https://www.twitter.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img alt="twitter logo" src={twitter} className={classes.icon} />
         </Grid>
-        <Grid item className={classes.gridItem}>
-          <Grid container direction="column" spacing={2}>
-            <Grid item component={Link} to="/" className={classes.link}>
-              About Us
-            </Grid>
-            <Grid item component={Link} to="/" className={classes.link}>
-              History
-            </Grid>
-            <Grid item component={Link} to="/" className={classes.link}>
-              Team
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item className={classes.gridItem}>
-          <Grid container direction="column" spacing={2}>
-            <Grid item className={classes.link}>
-              Contact Us
-            </Grid>
-          </Grid>
+        <Grid
+          item
+          component={'a'}
+          href="https://www.instagram.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img alt="instagram logo" src={instagram} className={classes.icon} />
         </Grid>
       </Grid>
-      <img alt="icecone" className={classes.adorement} src={logo}></img>
     </footer>
   );
 }
