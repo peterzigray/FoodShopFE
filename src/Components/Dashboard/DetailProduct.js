5d232d6dc57765234187ada3ad4d97a0b593aeb3
splitSlice <zigray.peter@gmail.com>
2020-05-04 23:32:39 +0200

    second detail page layout done

--- src/Components/Dashboard/DetailProduct.js
+++ src/Components/Dashboard/DetailProduct.js
@@ -6,7 +6,9 @@ import List from '@material-ui/core/List';
 import Badge from '@material-ui/core/Badge';
 import EcoIcon from '@material-ui/icons/Eco';
 import ListIcon from '@material-ui/icons/List';
-
+import instagram from '../../assets/instagram.svg';
+import facebook from '../../assets/facebook.svg';
+import twitter from '../../assets/twitter.svg';
 import Rating from '@material-ui/lab/Rating';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 
@@ -56,7 +58,26 @@ import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  leftItem: {
+    alignSelf: 'flex-end',
+  },
+  priceArea: { backgroundColor: '#fff59d', display: 'flex' },
+  cardCont: { display: 'flex', paddingBottom: '0px', paddingTop: '0px' },
+  cardCont2: { display: 'flex', paddingTop: '0px' },
+  elevation: { boxShadow: ' 0px 0px 0px 0px' },
+  mainContainer: { marginTop: '5rem', marginBottom: '10rem' },
+  secondGrid: { height: '25em' },
+  icon: {
+    height: '1.4em',
+
+    width: '1.8em',
+    [theme.breakpoints.down('xs')]: {
+      height: '1em',
+      width: '1em',
+    },
+  },
+}));
 
 const DetailProduct = ({
   products: { products, loading },
@@ -66,9 +87,9 @@ const DetailProduct = ({
   const [value, setValue] = useState(null);
   const [arrow, setArrow] = useState(false);
 
-  // useEffect(() => {
-  //   getDetailProduct(1, history);
-  // }, []);
+  useEffect(() => {
+    getDetailProduct(1, history);
+  }, []);
 
   const classes = useStyles();
 
@@ -81,16 +102,116 @@ const DetailProduct = ({
   function Layout() {
     return (
       <Fragment>
-        <Grid container>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          className={classes.mainContainer}
+        >
           <Grid item xs={6}>
-            {/* {products.map((product) => ( */}
-            <Card classes={{ root: classes.elevation }} position="relative">
-              <img height="450px" width="450px" src={products.imageUrl}></img>
-            </Card>
-            {/* ))} */}
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              className={classes.secondGrid}
+            >
+              <Grid item xs={5}>
+                {/* {products.map((product) => ( */}
+
+                <img height="500em" width="500em" src={products.imageUrl}></img>
+
+                {/* ))} */}
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            right
+          <Grid item xs={4}>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              className={classes.secondGrid}
+            >
+              <Grid item>
+                <Card classes={{ root: classes.elevation }}>
+                  <CardContent className={classes.cardCont}>
+                    <Typography variant="h2" color="primary" gutterBottom>
+                      {products.name}
+                    </Typography>
+                  </CardContent>
+                  <CardContent className={classes.cardCont2}>
+                    <Rating
+                      name="simple-controlled"
+                      value={products.rating}
+                      // onChange={(event, newValue) => {
+                      //   setValue(newValue);
+                      // }}
+                    />
+                    <Grid
+                      item
+                      component={'a'}
+                      href="https://www.facebook.com"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <img
+                        alt="facebook logo"
+                        src={facebook}
+                        className={classes.icon}
+                      />
+                      <Grid
+                        item
+                        component={'a'}
+                        href="https://www.twitter.com"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <img
+                          alt="twitter logo"
+                          src={twitter}
+                          className={classes.icon}
+                        />
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                  <CardContent>
+                    <Typography>
+                      Cheesecake liquorice sweet roll jujubes dragée liquorice
+                      sugar plum. Soufflé dessert tart liquorice cotton candy
+                      cheesecake bear claw powder cupcake. Macaroon dessert
+                      danish donut jelly-o powder candy marshmallow soufflé.
+                      Carrot cake cake marshmallow pie cake pie candy canes
+                      topping. Chocolate donut jelly beans. Sweet lollipop
+                      topping. Chupa chups wafer candy topping sweet roll toffee
+                      halvah. Biscuit gummies candy canes icing chupa chups
+                      apple pie sesame snaps pie. Wafer donut chocolate candy
+                      canes. Marzipan pudding fruitcake jelly beans oat cake
+                      cotton candy cake. Muffin marzipan oat cake pudding
+                      pudding lemon drops gummi bears. Icing gummies marshmallow
+                      cake caramels chocolate cake.
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Box className={classes.priceArea}>
+                      <Typography variant="h2">{'Price: '}</Typography>
+                      <Typography variant="h2">
+                        {products.price}
+                        {'$'}
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                  <CardContent>
+                    <Box>
+                      <Button variant="contained" size="large" color="primary">
+                        Buy
+                      </Button>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Fragment>
